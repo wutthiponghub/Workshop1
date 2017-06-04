1a3f2dd970ccdb118a79303c93b1a975c811373b
Wutthipong Chinnasri <wutthipong.c@rsu.ac.th>
2017-06-04 22:10:34 +0700

    Routing in AngularJS

---
+++ app.js
@@ -0,0 +1,18 @@
+var app = angular.module("myApp", ['ngRoute']);
+
+app.config(function($routeProvider, $locationProvider) {
+    $locationProvider.html5Mode(true);
+    $routeProvider
+        .when("/", {
+            templateUrl: "page1/page1.html",
+            controller: "page1Controller"
+        })
+        .when("/page1", {
+            templateUrl: "page1/page1.html",
+            controller: "page1Controller"
+        })
+        .when("/page2", {
+            templateUrl: "page2/page2.html",
+            controller: "page2Controller"
+        });
+});
