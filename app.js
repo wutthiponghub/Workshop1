8b8decbf9e5a39eadcf2f802d953b8622848720d
Wutthipong Chinnasri <wutthipong.c@rsu.ac.th>
2017-06-04 22:12:55 +0700

    Active Menu

--- app.js
+++ app.js
@@ -3,10 +3,10 @@ var app = angular.module("myApp", ['ngRoute']);
 app.config(function($routeProvider, $locationProvider) {
     $locationProvider.html5Mode(true);
     $routeProvider
-        .when("/", {
-            templateUrl: "page1/page1.html",
-            controller: "page1Controller"
-        })
+    // .when("/", {
+    //     templateUrl: "page1/page1.html",
+    //     controller: "page1Controller"
+    // })
         .when("/page1", {
             templateUrl: "page1/page1.html",
             controller: "page1Controller"
@@ -15,4 +15,10 @@ app.config(function($routeProvider, $locationProvider) {
             templateUrl: "page2/page2.html",
             controller: "page2Controller"
         });
+});
+
+app.controller('navController', function($scope, $location) {
+    $scope.isActive = function(viewLocation) {
+        return viewLocation === $location.path();
+    };
 });
